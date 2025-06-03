e70e3730dc0bf987feef4e9cd19bf597303126aa
Bhasha-4545 <emahaboob236@gmail.com>
2025-06-03 15:50:35 +0530

    Add files via upload

---
+++ SecondwayStack.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+public class SecondwayStack{
+	public static void main(String[] args) {
+		Stack s = new stack<>();
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		System.out.println(s.peek());
+		System.out.println(s.pop());
+		System.out.println(s);
+    }
+}
